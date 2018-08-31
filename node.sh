fdc6a22821ddbabf484f650bea37fe76cbeb9bd3
Anders Quist <anders.quist@stena.com>
2018-08-31 14:56:22 +0200

    Hopefully fixed so that nvm will work.

--- node.sh
+++ node.sh
@@ -2,6 +2,10 @@
 
 # nvm should have been installed by brew...
 
+mkdir ~/.nvm
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+
 nvm install 8.11.4
 nvm use 8.11.4
 nvm alias default 8.11.4
